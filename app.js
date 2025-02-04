0ed6377d948378023defc9ddd6a3b2e385b76110
Giovanni faustino <jovasfaustino@gmail.com>
2025-02-03 22:22:01 -0300

    Modificação teste

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
 }
 
 exibirMensagemInicial();
